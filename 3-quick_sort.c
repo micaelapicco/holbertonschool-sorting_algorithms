aaaec0b0bf2d74ee377f254c86845c386f77d202
root <matiassotelo171097@gmail.com>
2023-05-23 10:02:11 -0700

    quicksort v2

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,21 @@
 #include "sort.h"
 
 /**
+ *swapArray - a function to swap to numbers in an array 
+ *@a1: the first value to swap
+ *@a2: the second value to swap
+ */
+void swapArray(int *array, int a1, int a2)
+{
+size_t temp;
+
+temp = array[a1];
+array[a1] = array[a2];
+array[a2] = temp;
+}
+
+
+/**
  *quick_sort - a function that sorts using Quick sort algorithm
  *@array: the array to be sorted
  *@size: the size of the array
@@ -8,18 +23,16 @@
 
 void quick_sort(int *array, size_t size)
 {
-    size_t j = 0, i = j - 1, temp;
+    size_t j = 0, i = j - 1, pivot = size;
 
     if (array)
     {
-        for (j = 0; i < size - 1; j++)
+        for (j = 0; j < size - 1; j++)
         {
-            if (array[size] < array[j])
+            if (array[pivot] > array[j])
             {
                 i++;
-                temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
+                swapArray(array,i, j);
                 print_array(array, size);
             }
         }
